2400ac57bf51a219e3c391c1f8872f6fc105d6ae
tw-313 <tak7901y298@gmail.com>
2022-02-11 00:47:13 +0900

    6.3.3 パスワードの最小文字数

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ApplicationRecord
     validates :email, presence: true, length: { maximum: 255 },
                       format: { with: VALID_EMAIL_REGEX },
                       uniqueness: { case_sensitive: false}
-  end
+    has_secure_password
+    validates :password, presence: true, length: { minimum: 6 }
+
+
+
+end
