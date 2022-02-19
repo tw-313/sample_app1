2f7cd0472270de5e57e520ab9cd0dc156ea9b354
tw-313 <tak7901y298@gmail.com>
2022-02-19 22:05:02 +0900

    8.1.3ユーザーの検索と認証 finish

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
+
   def new
   end
+
+  def create
+    user = User.find_by(email; params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      #ユーザーログイン後にユーザー情報のページにリダイする
+    else
+      #エラーメッセ時を作成する
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+
 end
