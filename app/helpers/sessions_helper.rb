8a0359f1982fc141a43a26b8e52b1d3119a6f883
tw-313 <tak7901y298@gmail.com>
2022-02-22 23:09:54 +0900

    8.2.3レイアウトリンクを変更する finish

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,7 +7,12 @@ module SessionsHelper
     # 現在ログイン中のユーザーを返す
     def current_user
         if session[:user_id]
-            @current_user || = User.find_by(id: session[:user_id])
+            @current_user ||= User.find_by(id: session[:user_id])
         end
+    end
+
+    #ユーザーがログインしていればtrue その他ならfalseを返す
+    def logged_in?
+        !current_user.nil?
     end
 end
