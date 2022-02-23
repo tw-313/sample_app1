980dbd9cdc4d88222e8b545205650cf75848f2a8
tw-313 <tak7901y298@gmail.com>
2022-02-23 21:50:14 +0900

    8.2.4　レイアウトの変更をテストする#

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,7 +2,11 @@ require "test_helper"
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
 
-  test "login with invalid information" do
+  def setup
+    @user = users(:michael)
+  end
+
+  test "login with valid information" do
     get login_path
     assert_template 'sessions/new'
     post login_path, params: { session: {email: "", password: ""}}
