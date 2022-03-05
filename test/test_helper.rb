11051dba559b23849c3a109158f2578b37d1ab53
tw-313 <tak7901y298@gmail.com>
2022-03-05 22:57:34 +0900

    10.2.1ユーザーにログインを要求する finish

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -21,5 +21,11 @@ class ActiveSupport::TestCase
     @current_user = nil
   end
 
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
+  end
+
   # Add more helper methods to be used by all tests here...
 end
