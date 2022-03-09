3e99a091cd69723f8e9de076060379de475dde70
tw-313 <tak7901y298@gmail.com>
2022-03-09 23:34:34 +0900

    修正

--- test/helpers/application_helper_test.rb
+++
@@ -1,9 +0,0 @@
-
-require 'test_helper'
-
-class ApplicationTest < ActionView::TestCase
-    test "full title helper" do
-        assert_equal full_title, "Ruby on Rails Tutorial Sample App"
-        assert_equal full_title("Help"), "Help | Ruby on Rails Tutorial Sample App"
-    end
-end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,31 +1,31 @@
 ENV['RAILS_ENV'] ||= 'test'
-require_relative "../config/environment"
-require "rails/test_help"
+require_relative '../config/environment'
+require 'rails/test_help'
 require "minitest/reporters"
 Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
-  # Run tests in parallel with specified workers
+  # 特定のワーカーではテストをパラレル実行する
   parallelize(workers: :number_of_processors)
 
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-  include ApplicationHelper
 
   def is_logged_in?
     !session[:user_id].nil?
   end
 
-  def log_out
-    session.delete(:user_id)
-    @current_user = nil
+  # テストユーザーとしてログインする
+  def log_in_as(user)
+    session[:user_id] = user.id
   end
+end
 
+class ActionDispatch::IntegrationTest
+
+  # テストユーザーとしてログインする
   def log_in_as(user, password: 'password', remember_me: '1')
     post login_path, params: { session: { email: user.email,
                                           password: password,
                                           remember_me: remember_me } }
   end
-
-  # Add more helper methods to be used by all tests here...
 end
